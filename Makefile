afd1666b65e262494cf0ebeb16b08255e2b8addd
Irek Szcześniak <irek.szczesniak@gmail.com>
2017-07-24 10:25:24 +0200

    args

---
+++ Makefile
@@ -0,0 +1,45 @@
+TARGETS = ad info
+TARGET_OBJS = $(addsuffix .o, $(TARGETS))
+
+OBJS = cli_args.o client.o connection.o constraints.o routing.o		\
+stats.o utils.o utils_netgen.o mypoint.o teventqueue.o traffic.o
+
+CXXFLAGS := $(CXXFLAGS) -g
+CXXFLAGS := $(CXXFLAGS) -std=c++14
+CXXFLAGS := $(CXXFLAGS) -I include
+
+# Use the C++ linker
+LINK.o = $(LINK.cc)
+
+ifdef BOOST_ROOT
+	CXXFLAGS := $(CXXFLAGS) -I $(BOOST_ROOT)/include
+	LDFLAGS := $(LDFLAGS) -L $(BOOST_ROOT)/lib
+endif
+
+LDFLAGS := $(LDFLAGS) -l boost_program_options
+LDFLAGS := $(LDFLAGS) -l boost_timer
+LDFLAGS := $(LDFLAGS) -l boost_system
+
+all: $(TARGETS)
+
+ad: $(OBJS)
+
+info: $(OBJS)
+
+try: graph.o try.o
+
+.PHONY: clean count depend test
+
+clean:
+	rm -f *~
+	rm -f $(OBJS)
+	rm -f $(TARGET_OBJS)
+	rm -f $(TARGETS)
+
+count:
+	wc -l *.hpp *.cc
+
+depend:
+	g++ $(CXXFLAGS) -MM *.cc > dependencies
+
+include dependencies
---
+++ cli_args.hpp
@@ -0,0 +1,83 @@
+#ifndef CLI_ARGS_HPP
+#define CLI_ARGS_HPP
+
+#include "connection.hpp"
+#include "routing.hpp"
+
+#include <string>
+#include <boost/optional.hpp>
+
+using namespace std;
+
+/**
+ * These are the program arguments.  In this single class we store all
+ * information passed at the command line.
+ */
+struct cli_args
+{
+  /// -----------------------------------------------------------------
+  /// The network options
+  /// -----------------------------------------------------------------
+  
+  /// The network type.
+  std::string nt;
+
+  /// The number of nodes in the graph.
+  int nr_nodes;
+
+  /// The number of edges in the graph.
+  boost::optional<int> nr_edges;
+
+  /// The number of slices.
+  int nr_sc;
+
+  /// The maximal length of a path we want.
+  boost::optional<COST> ml;
+
+  /// The maximal length coefficient of a path we want.
+  boost::optional<float> mlc;
+
+  /// The K for the k-shortest paths.
+  boost::optional<unsigned> K;
+
+  /// The spectrum selection type.
+  std::string st;
+
+  /// -----------------------------------------------------------------
+  /// The traffic options
+  /// -----------------------------------------------------------------
+
+  /// The mean client arrival time.
+  double mcat;
+
+  /// The offered load.
+  double ol;
+
+  /// The mean holding time.
+  double mht;
+
+  /// The mean number of slices.
+  double mnsc;
+
+  /// -----------------------------------------------------------------
+  /// The simulation options
+  /// -----------------------------------------------------------------
+
+  /// The seed
+  int seed;
+
+  /// The hash of all the parameters except the seed parameter.  It's
+  /// used to identify the configuration of the simulation.
+  string hash;
+
+  /// The limit on the simulation time.
+  double sim_time;
+};
+
+/**
+ * This function parses the command-line arguments.
+ */
+cli_args
+process_cli_args(int argc, const char* argv[]);
+
+#endif /* CLI_ARGS_HPP */
